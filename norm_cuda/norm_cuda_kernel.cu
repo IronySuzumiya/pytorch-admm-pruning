cadbd27c6e13ceb24c1151ebaf7cbef38c89aabf
IronySuzumiya <811073137@qq.com>
2020-01-10 16:48:02 +0800

    cuda norm

--- norm_cuda/norm_cuda_kernel.cu
+++ norm_cuda/norm_cuda_kernel.cu
@@ -13,8 +13,8 @@ __global__ void norm_cuda_kernel(torch::PackedTensorAccessor32<scalar_t, 2, torc
                           int Nx,
                           int Ny )
 {
-  int startX = blockIdx.x * 8 + threadIdx.x * ou_w;
-  int startY = blockIdx.y * 8 + threadIdx.y * ou_h;
+  int startX = (blockIdx.x * 8 + threadIdx.x) * ou_w;
+  int startY = (blockIdx.y * 8 + threadIdx.y) * ou_h;
   if (startX < Wx && startY < Wy) {
     scalar_t sum = 0;
     for (int i = 0; i < ou_w && startX + i < Wx; i++) {
